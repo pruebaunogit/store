3753e3b779ecbefc3b7edb6126b822df00090e45
pruebaunogit <pruebaunocorreelectronico@gmail.com>
2024-04-25 20:56:17 -0500

    Eventos 2 - calculo de subtotal

--- scripts/productDetail.js
+++ scripts/productDetail.js
@@ -5,7 +5,7 @@ const id = params.get('id');
 
 function printDetails(id) {
     const product = products.find((each) => each.id === id);
-    console.log(product)
+    // console.log(product)
     const detailsTemplate = `
     <div class="product-images-block">
         <div class="thumbnail-container"> 
@@ -13,31 +13,37 @@ function printDetails(id) {
                 each => `<img class="miniImg" src="${each}" alt="mini" />`
             ).join("")} 
         </div>
-        <img class="main-image" src="${product.images[0]} " alt="macbok img1" />
+        <img id="main-image" class="main-image" src="${product.images[0]} " alt="macbok img1" onclick="changeMini(event)"/>
     </div>
     <div class="product-description-block">
         <h1 class="title">${product.title}</h1>
         <form class="selector">
-        <fieldset>
-            <label class="label" for="color">Color</label>
-            <select id="color" type="text" placeholder="Selecciona un color">
-            ${product.colors.map(
-                (each) => `<option value=${each}>${each}</option>`
-             ).join("")}
-            </select>
-        </fieldset>
+            <fieldset>
+                <label class="label" for="color">Color</label>
+                <select id="color" type="text" placeholder="Selecciona un color">
+                ${product.colors.map(
+                    (each) => `<option value=${each}>${each}</option>`
+                ).join("")}
+                </select>
+            </fieldset>
         </form>
+        <div style="width: 100%; display: flex; align-items: center;">
+            <h2 style="margin-right: 10px;">Precio:</h2>
+            <p style="margin-left: auto;">
+                <span style="text-align: right;">${"$"+product.price}</span>
+            </p>
+        </div>
         <div class="description">
-        <h1>Descripción:</h1>
-        <p>
-            ${product.description}
-        </p>
+            <h2>Descripción:</h2>
+            <p>
+                ${product.description}
+            </p>
         </div>
     </div>
     <div class="product-checkout-block">
         <div class="checkout-container">
         <span class="checkout-total-label">Total:</span>
-        <h2 class="checkout-total-price">${"$"+product.price}</h2>
+        <h2 id="checkout-total-price" class="checkout-total-price">${"$"+product.price}</h2>
         <p class="checkout-description">
             Incluye impuesto PAIS y percepción AFIP. Podés recuperar AR$
             50711 haciendo la solicitud en AFIP.
@@ -64,7 +70,7 @@ function printDetails(id) {
         </ul>
         <div class="checkout-process">
             <div class="top">
-            <input id="txt_comprar" type="number" value="1" />
+            <input id="txt_comprar" type="number" value="1" onchange="changeSubtotal(this)"/>
             <button id="btn_comprar" class="btn-primary">Comprar</button>
             </div>
             <div class="bottom">
@@ -78,3 +84,21 @@ function printDetails(id) {
     detailsSelector.innerHTML = detailsTemplate;
 }
 printDetails(id)
+
+
+function changeMini(event) {
+    const selectedSrc = event.target.src;
+    // const bigSelector = document.querySelector("#bigImg");
+    const bigSelector = document.querySelector("#main-image");
+    bigSelector.src = selectedSrc;
+}
+    
+function changeSubtotal(c){
+    // console.log(c.value);
+    // console.log(c);
+    const cant_prod = c.value >=0 ? c.value : 0;
+    const product = products.find((each) => each.id === id);
+    const subTotal = product.price * cant_prod;
+    document.querySelector("#checkout-total-price").textContent = "$"+subTotal;
+    
+}
